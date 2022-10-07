971af3891d7ad06cd7942c82c2ff12788adfb64e
Maxim <36982547+Siphoin@users.noreply.github.com>
2022-10-07 20:33:14 +0500

    first commit

---
+++ SNEngine/SNEngine/GameWindow.cs
@@ -0,0 +1,51 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace SNEngine;
+public class GameWindow
+{
+
+    public event Action OnClosed;
+
+     private RenderWindow _window;
+      public GameWindow (string title = "New SNEngine Novel")
+    {
+        VideoMode video = new VideoMode(600, 600);
+
+        Color color = Color.Green;
+
+        _window = new RenderWindow(video, title);
+
+        StartWhile(color);
+    }
+
+    private void StartWhile(Color color)
+    {
+        _window.Closed += Close;
+
+        while (_window.IsOpen)
+        {
+            _window.DispatchEvents();
+
+            _window.Clear(color);
+
+            _window.Display();
+        }
+    }
+
+    private void Close(object? sender, EventArgs e)
+    {
+        OnClosed?.Invoke();
+
+
+        _window.Close();
+
+    }
+
+    public void SetTitle(string placeholder) 
+    {
+       _window.SetTitle(placeholder);
+
+       Console.WriteLine(string.Format("New title of window {0}", _window.ToString()));
+    }
+}
---
+++ SNEngine/SNEngineDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace SNEngine.Demo
+{
+    public class Program
+    {
+
+        private static void Main(string[] args) 
+        {
+             GameWindow window = new GameWindow("SNEngine Demo");
+
+
+        }
+    }
+}
