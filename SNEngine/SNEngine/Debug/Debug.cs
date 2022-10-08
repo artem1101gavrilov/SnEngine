3b2b20eff1482ca8c1d59d9864f3be156d857add
Maxim <36982547+Siphoin@users.noreply.github.com>
2022-10-08 15:38:15 +0500

    add preprocessors to Debug.cs

--- SNEngine/SNEngine/Debug/Debug.cs
+++ SNEngine/SNEngine/Debug/Debug.cs
@@ -4,11 +4,13 @@ namespace SNEngine
     {
         public static void Log (string message)
         {
+            #if DEBUG
             SetColorConsole(ConsoleColor.Blue);
 
             Console.WriteLine($"[SNEngine Message]: {message}");
 
             ResetColor();
+            #endif
         }
 
         private static void ResetColor() =>  Console.ResetColor();
@@ -22,32 +24,42 @@ namespace SNEngine
              Console.WriteLine($"[SNEngine ERROR]: {message}");
 
               ResetColor();
+
         }
 
         public static void LogWarning (string message) 
         {
+            #if DEBUG
              SetColorConsole(ConsoleColor.Yellow);
 
              Console.WriteLine($"[SNEngine Warning]: {message}");
              
               ResetColor();
+
+              #endif
         }
         public static void LogAction (string message) 
         {
+            #if DEBUG
              SetColorConsole(ConsoleColor.Green);
 
              Console.WriteLine($"[SNEngine Action]: {message}");
 
               ResetColor();
+
+            #endif
         }
 
         public static void LogSpecial (string message, ConsoleColor color) 
         {
+            #if DEBUG
              SetColorConsole(color);
 
              Console.WriteLine($"[SNEngine Special Event]: {message}");
 
               ResetColor();
+
+              #endif
         }
 
         
